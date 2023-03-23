a22eb4e6f4e1d9ef4c9f35dd801222f744b6e251
blaine <chunkypanda03@gmail.com>
2023-03-23 01:52:43 -0400

    fixed expression

--- EIF.py
+++ EIF.py
@@ -1,16 +1,15 @@
 import sympy
 from sympy import *
+from sympy.solvers.solvers import _tsolve as tsolve
 import numpy as np
-from sympy.abc import x, y
+from sympy.abc import E, x,y
 from scipy.optimize import root_scalar,fsolve
 m = 511000
-E=1
+U=1
+L=2
 h_ = (.658)*10**-15
-express1 = sympy.tan(sympy.sqrt((2*m*E)/(h_**2))*x)
-express2 = (-sympy.sqrt(E/(x-E)))
-#express1= sympy.cos(x)
-#express2= sympy.sin(sympy.sqrt((5*x)/(4*x)**2))
-#domain = [1 < x, x <= 2]
-#eq1 = Eq(express1, express2)
-#print(str(sympy.tan(sympy.sqrt((2*m*E)/(h_**2))*1.847)))
-print(solve([Eq(express2,express1)], x, domain=[1,2]), particular = True, quick= True)
+express1 = sympy.tan(sympy.sqrt((2*m*E)/(h_**2))*L)
+express2 = (-sympy.sqrt(E/(U-E)))
+express3 = (sympy.tan(sympy.sqrt((2*m*E)/(h_**2))*L)) + sympy.sqrt(E/(U-E))
+soln = solve(express1 -express2, E)
+pprint(print(soln))
